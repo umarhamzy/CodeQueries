ab59e4dcafd6d3ba49ebe13ba59ce868ff94c984
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2023-10-26 12:55:30 +0530

    Add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Here we&apos;re come Nextjs!</h1>
-      <h1 className="h2-bold">Here we&apos;re come Nextjs!</h1>
-      <h1 className="h3-bold">Here we&apos;re come Nextjs!</h1>
-    </div>
-  )
-}
