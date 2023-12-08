4eb375dce4c1dd4c411e89efa88e73d0c5e65b2b
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2023-12-08 21:51:44 +0530

    Adjust HomeFilter styling

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -14,8 +14,8 @@ const HomeFilters = () => {
           onClick={() => {}}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active == filter.value
-              ? "bg-primary-100 text-primary-500 dark:bg-dark-200"
-              : "bg-light-800 text-light-500 dark:bg-dark-200 dark:text-light-500"
+              ? "bg-primary-100 text-primary-500 dark:bg-dark-400"
+              : "bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-200 dark:text-light-500 dark:hover:bg-dark-400"
           }`}
         >
           {filter.name}
