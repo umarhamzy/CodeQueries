5c8c56189dd6c63b1dee914462abb6a4b3a977f5
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2023-12-21 20:33:35 +0530

    fix eslint errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -67,9 +67,9 @@ export async function deleteUser(params: DeleteUserParams) {
     // TODO: Delete user from database and questions, answers, comments etc..
 
     // get user question ids
-    const userQuestionsIds = await Question.find({ author: user._id }).distinct(
-      "_id",
-    );
+    // const userQuestionsIds = await Question.find({ author: user._id }).distinct(
+    //   "_id",
+    // );
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
