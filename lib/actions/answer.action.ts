fc305cf348df39ce936613841f95b87905fe6502
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2024-01-13 13:17:11 +0530

    displays posted answers in questions page

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import Answer from "@/database/answer.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { content, author, question, path } = params;
+
+    // Create a document (entry) in the "answer" collection in database
+    const newAnswer = await Answer.create({
+      content,
+      author,
+      question,
+    });
+
+    // console.log({newAnswer})
+
+    // Add the answer to the question's answer array
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+
+    // TODO: Add interaction..
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    const answers = await Answer.find({ question: questionId })
+      .populate("author", "_id clerkId name picture")
+      .sort({ createdAt: -1 });
+
+    return { answers };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
